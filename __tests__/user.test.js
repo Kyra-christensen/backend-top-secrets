a04a1e5acc484a76ffa34849c0f1eff0d5be3ea9
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-30 15:33:46 -0700

    created signs in a user test

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secret routes', () => {
   beforeEach(() => {
@@ -13,7 +14,7 @@ describe('top-secret routes', () => {
   });
 
   it('signs up a user', async () => {
-    const newUser = { email: 'kyra@email.com', password: 'secretpassword' };
+    const newUser = { email: 'kyra@email.com', password: 'totallysecretpassword' };
 
     const res  = await request(app)
       .post('/api/v1/users')
@@ -22,5 +23,17 @@ describe('top-secret routes', () => {
     expect(res.body).toEqual({ id: expect.any(String), email: 'kyra@email.com' });
   });
 
-  
+  it('signs in a user', async () => {
+    const userData = { email: 'kyra@email.com', password: 'totallysecretpassword' };
+
+    const user = await UserService.create(userData);
+
+    const agent = request.agent(app);
+
+    const res = await agent
+      .post('/api/v1/users/sessions')
+      .send(userData);
+    
+    expect(res.body).toEqual({ message: 'You signed in successfully!', user });
+  });
 });
